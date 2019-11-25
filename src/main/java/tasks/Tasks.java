78124b3686dbcba0afdd7d1649d67d80f0b2a58d
magnp <nastya.mihnovich@mail.ru>
2019-11-25 14:12:54 +0300

    five tasks tests

--- src/main/java/tasks/Tasks.java
+++ src/main/java/tasks/Tasks.java
@@ -2,8 +2,57 @@ package tasks;
 
 public class Tasks {
 
+//    Given two strings, a and b, return the result of putting them together in the order abba,
+//    e.g. "Hi" and "Bye" returns "HiByeByeHi".
     public static String makeAbba(String a, String b) {
         return a + b + b + a;
+    }
+
+
+//   Return true if the given string contains a "bob" string, but where the middle 'o' char can be any char.
+//    bobThere("b9b") → true
+    public static boolean bobThere(String str) {
+        for (int i = 0; i <= str.length()-3; i++){
+            if (str.substring(i,i+1).equals("b")&&str.substring(i+2,i+3).equals("b")){
+                return true;
+            }
+        }
+        return false;
+    }
+
+//  Return true if the string "cat" and "dog" appear the same number of times in the given string.
+//  catDog("catdog") → true
+//  catDog("catcat") → false
+    public static boolean catDog(String str) {
+        int count_cat = 0;
+        int count_dog = 0;
+        for (int i = 0; i < str.length()-2; i++){
+            if (str.substring(i,i+3).equals("cat")){
+                count_cat++;
+            }
+            if (str.substring(i,i+3).equals("dog")){
+                count_dog++;
+            }
+        }
+
+        return count_cat == count_dog;
+    }
+
+
+//   Given a string, return a string where for every char in the original, there are two chars.
+//   doubleChar("The") → "TThhee"
+    public static String doubleChar(String str) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i< str.length(); i++){
+            res.append(str.charAt(i)).append(str.charAt(i));
+        }
+        return res.toString();
+    }
+
+//   Given 2 strings, return their concatenation, except omit the first char of each. The strings will be at least length 1.
+//   nonStart("Hello", "There") → "ellohere"
+    public static String nonStart(String a, String b) {
+        return a.substring(1)+b.substring(1);
     }
 
 
--- src/test/java/tasks/TasksTest.java
+++ src/test/java/tasks/TasksTest.java
@@ -12,8 +12,8 @@ public class TasksTest {
 
         String a = "aaa";
         String b = "bbb";
-
         String expected = "aaabbbbbbaaa";
+
         String result = Tasks.makeAbba(a, b);
 
         assertEquals(expected, result);
@@ -24,8 +24,8 @@ public class TasksTest {
 
         String a = null;
         String b = null;
-
         String expected = "nullnullnullnull";
+
         String result = Tasks.makeAbba(a, b);
 
         assertEquals(expected, result);
@@ -36,8 +36,8 @@ public class TasksTest {
 
         String a = "";
         String b = "";
-
         String expected = "";
+
         String result = Tasks.makeAbba(a, b);
 
         assertEquals(expected, result);
@@ -48,8 +48,8 @@ public class TasksTest {
 
         String a = "";
         String b = "kek";
-
         String expected = "kekkek";
+
         String result = Tasks.makeAbba(a, b);
 
         assertEquals(expected, result);
@@ -60,10 +60,164 @@ public class TasksTest {
 
         String a = "kek";
         String b = "";
-
         String expected = "kekkek";
+
         String result = Tasks.makeAbba(a, b);
 
         assertEquals(expected, result);
     }
+
+    @Test
+    public void bobThere(){
+        String a = "bbb";
+        boolean expected = true;
+
+        boolean result = Tasks.bobThere(a);
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void bobThereNoBob(){
+        String a = "abcdegdhaoldbckspw";
+        boolean expected = false;
+
+        boolean result = Tasks.bobThere(a);
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void bobThereEmpty(){
+        String a = "";
+        boolean expected = false;
+
+        boolean result = Tasks.bobThere(a);
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void bobThereNumbers(){
+        String a = "bb12313b1b1b0b";
+        boolean expected = true;
+
+        boolean result = Tasks.bobThere(a);
+
+        assertEquals(expected,result);
+    }
+
+    @Test
+    public void catDog(){
+        String a = "catdog";
+        boolean expected = true;
+
+        boolean result = Tasks.catDog(a);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void catDogSeveralTimes(){
+        String a = "catdogcatcatdogcatdoglaladog";
+        boolean expected = true;
+
+        boolean result = Tasks.catDog(a);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void catDogEmpty(){
+        String a = "";
+        boolean expected = true;
+
+        boolean result = Tasks.catDog(a);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void catDogNone(){
+        String a = "cdc";
+        boolean expected = true;
+
+        boolean result = Tasks.catDog(a);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void doubleChar(){
+        String a = "hi";
+        String expected = "hhii";
+
+        String result = Tasks.doubleChar(a);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void doubleCharEmpty(){
+        String a = "";
+        String expected = "";
+
+        String result = Tasks.doubleChar(a);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void doubleCharDots(){
+        String a = "...";
+        String expected = "......";
+
+        String result = Tasks.doubleChar(a);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void doubleCharOneChar(){
+        String a = "o";
+        String expected = "oo";
+
+        String result = Tasks.doubleChar(a);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void nonStart(){
+        String a ="LOL";
+        String b = "kek";
+        String expected = "OLek";
+
+        String result = Tasks.nonStart(a,b);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void nonStartOneCharStringA(){
+        String a ="a";
+        String b = "bcd";
+        String expected = "cd";
+
+        String result = Tasks.nonStart(a,b);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    public void nonStartOneCharStringB(){
+        String a ="boogaga";
+        String b = "k";
+        String expected = "oogaga";
+
+        String result = Tasks.nonStart(a,b);
+
+        assertEquals(expected, result);
+    }
+
 }
